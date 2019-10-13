49302f59a2bf3f707765586148238ac991a35423
Yuhao He <yhee0025@student.monash.edu>
2019-10-13 22:13:10 +1100

    sdgs

--- FIT5032_A2/Controllers/HomeController.cs
+++ FIT5032_A2/Controllers/HomeController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mail;
 using System.Web.Mvc;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
 
 namespace FIT5032_A2.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -26,5 +31,47 @@ namespace FIT5032_A2.Controllers
 
             return View();
         }
+        // GET: Home
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SendEmail(Models.EmailModel model)
+        {
+            using (System.Net.Mail.MailMessage mm = new System.Net.Mail.MailMessage(model.Email, model.To))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                if (model.Attachment.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(model.Attachment.FileName);
+                    mm.Attachments.Add(new Attachment(model.Attachment.InputStream, fileName));
+                }
+                mm.IsBodyHtml = false;
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential(model.Email, model.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+                    ViewBag.Message = "Email sent.";
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult PrintContact(int? id)
+        {
+            var report = new Rotativa.ActionAsPdf("Contact", new { id = id });
+            return report;
+        }
+
     }
 }
